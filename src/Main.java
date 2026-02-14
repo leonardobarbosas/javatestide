f04da962e3db9585e210de1e2d608576bfa362e7
leonardobarbosas <leonardobarbosantos@gmail.com>
2026-02-13 22:10:16 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import model.Carro;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Carro carro = new Carro("Civic", "Honda", 2025);
+        System.out.println(carro.toString());
+
+    }
+}
